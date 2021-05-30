39dc09a528d9b86cd08355f6bed7836cdd241783
Ghosoon Sbaih <ghsoonsbaih96@gmail.com>
2021-05-30 14:37:51 +0300

    new extension

--- cypress/integration/Buydress/Buydress.ts
+++ cypress/integration/Buydress/Buydress.ts
@@ -1,24 +1,59 @@
-import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
-import homePageActions from '../pageObject/homePage/actions'
-const homePage=new homePageActions();
+import { Given, Then, When } from "cypress-cucumber-preprocessor/steps"
+import homePageActions from 'cypress/pageObject/homePage/actions'
+import homePageAssertions from 'cypress/pageObject/homePage/assertions'
+
+
+
 const url = 'http://automationpractice.com/index.php'
-Given('The user visits automationpractice page', () => {
+const homePage= new homePageActions()
+const homePageAss= new homePageAssertions()
+
+
+
+Given("The user visits automationPractice page", () => {
   cy.visit(url)
 })
 
-When('The user hover over the {string} item', ()=>
-
+When('The user hover over the \'Faded Short Sleeve T-shirts\' item', ()=>
 {
-  homePage.hoveroverMenuItem()
+  homePage.hoverOverMenuItem()
+  
+})
+
+When ('The user clicks on the \'Add to Cart\' button',()=>{
   homePage.clickAddToChartButton()
 })
 
-Then(`The user should see a popup opened with {string} title `,()=>{
-  cy.contains('{string}')
- /* cy.get('.button-medium > span').contains('Proceed to checkout').click()
-  cy.get('[class="shopping_cart"]').contains('Cart')
-*/
+Then('The user should see a popup opened with \'Product successfully added to your shopping cart\' title',()=>{
+  homePageAss.checkPopupModelIsOpen()
+  
+})
+
+
+
+Given("The user visits the automationPractice page",()=>{
+  
 
 })
+
+When ('The user clicks on the \'Add to Cart\' button',()=>{
+  
+})
+
+When('The Popup should be opened',()=>{
+  
+  
+})
+
+When('The user clicks on the \'Proceed to checkout\' button',()=>{
+  homePage.clickProceedCheckButton()
+  
+})
+Then('The user should see a new page with \'Your shopping cart\' title',()=>{
+  homePageAss.checkShoppingCartSummeryIsOpen()
+  
+})
+
+
 
 
---
+++ cypress/pageObject/homePage/assertions.ts
@@ -0,0 +1,14 @@
+    class homePageAssertions{
+
+    checkPopupModelIsOpen(){
+        return cy.get('.layer_cart_product > h2').should('contain','Product successfully added to your shopping cart');
+    
+}
+
+checkShoppingCartSummeryIsOpen(){
+        return cy.get('[class="navigation_page"]').should('contain','Your shopping cart');
+    }
+
+}
+
+export default homePageAssertions;
